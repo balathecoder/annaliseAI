17f39001aca109c03abf89b8b7a88bd174ad5707
balathecoder <bkmbala@gmail.com>
2023-01-16 10:30:31 +0530

    gitignore

--- oauth2.py
+++ oauth2.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, status
 from annaliseAI import JWT_token
 from fastapi.security import OAuth2PasswordBearer
-
+## for authentication
 auth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
